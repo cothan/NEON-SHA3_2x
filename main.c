3e3af720b09e79e83fb81d45f1fcc496845aef45
cothan <cothannguyen@gmail.com>
2020-11-07 02:34:25 -0500

    refactor

--- main.c
+++ main.c
@@ -5,31 +5,34 @@
 #include "fips202x2.h"
 #include "fips202.h"
 
-#define TESTS 10000000
-#define ROL(a, offset) ((a << offset) ^ (a >> (64-offset)))
-
+#define TESTS 1
+#define OUTLENGTH 100
+#define INLENGTH 123
 
 int main()
 {
-    v128 state[25];
-    uint64_t s[25];
-    v128 t;
-    uint64_t bla, blo;
-    srand(time(0));
+    // srand(time(0));
+    uint8_t out1[OUTLENGTH], out2[OUTLENGTH];
+    uint8_t in1[INLENGTH], in2[INLENGTH];
+    uint8_t out_gold[OUTLENGTH], in_gold[INLENGTH];
 
-    for (int i  =0; i < 25; i++)
+    uint64_t bla, blo;
+
+    for (int i = 0; i < INLENGTH; i++)
     {
         bla = rand();
         blo = rand();
-        s[i] = bla ^ blo;
-        t = vdupq_n_u64(bla ^ blo);
-        state[i] = t;
+        in1[i] = bla;
+        in2[i] = blo;
+        in_gold[i] = bla;
+        // in_gold[i] = blo;
     }
     long_long start, end;
 
     start = PAPI_get_real_cyc();
-    for (int j = 0; j < TESTS; j++){
-        neon_KeccakF1600_StatePermute(state);
+    for (int j = 0; j < TESTS; j++)
+    {
+        shake128x2(out1, out2, OUTLENGTH, in1, in2, INLENGTH);
     }
     end = PAPI_get_real_cyc();
     printf("NEON: %lld\n", end - start);
@@ -37,27 +40,40 @@ int main()
     start = PAPI_get_real_cyc();
     for (int j = 0; j < TESTS; j++)
     {
-        KeccakF1600_StatePermute(s);
+        shake128(out_gold, OUTLENGTH, in_gold, INLENGTH);
     }
     end = PAPI_get_real_cyc();
     printf("FIPS: %lld\n", end - start);
-        
 
-    v128 sum = vdupq_n_u64(0);
-    uint64_t ssum = 0;
-    for (int i = 0; i < 25; i++)
+    int check = 0;
+    uint8_t m, n;
+
+    for (int i = 0; i < OUTLENGTH; i += 8)
     {
-        sum += state[i];
-        ssum += s[i];
+        for (int j = i; j < i + 8; j++)
+        {
+            m = out_gold[j];
+            n = out1[j];
+            if (out_gold[j] != out1[j])
+            {
+                printf("![%2d] %2x != %2x\n", j,  m, n);
+                check++;
+            }
+            else
+            {
+                printf("+[%2d] %2x == %2x\n", j,  m, n);
+            }
+            
+        }
+
+        if (check == 8)
+        {
+            printf("ERROR! %d\n", i);
+            return 1;
+        }
     }
 
-    if (sum[1] != ssum)
-    {
-        printf("=====\n");
-        printf("%lx\n", sum[1]);
-        printf("%lx\n", ssum);
-        return 1;
-    }
+    printf("CORRECT!\n");
 
     return 0;
 }
