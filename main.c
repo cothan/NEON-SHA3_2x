50430f8867f9a4de76764c5f1c8676d92caba6a1
cothan <cothannguyen@gmail.com>
2020-11-07 15:32:53 -0500

    add benchmark for shake128 and shake256

--- main.c
+++ main.c
@@ -6,91 +6,148 @@
 #include "fips202.h"
 
 #define TESTS 1000000
-#define OUTLENGTH SHAKE128_RATE*10
-#define INLENGTH SHAKE128_RATE
-#define BRUTE 0
+#define OUTLENGTH 4096
+#define INLENGTH 4096
+#define BRUTE 1
 
-int main()
+int compare(uint8_t *out_gold, uint8_t *out1, uint8_t *out2, int ol)
 {
-    // srand(time(0));
+    int check = 0;
+    uint8_t m, n;
+
+    for (int i = 0; i < ol - 8; i += 8)
+    {
+        for (int j = i; j < i + 8; j++)
+        {
+            m = out_gold[j];
+            n = out1[j];
+            if (out_gold[j] != out1[j])
+            {
+                printf("![%2d] %2x != %2x  ?? %2x\n", j, m, n, out2[j]);
+                check++;
+            }
+        }
+
+        if (check)
+        {
+            printf("%d ERROR! %d\n", ol, i);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int bench(void func(), void funcx2(),
+           uint8_t *out_gold, uint8_t *out1, uint8_t *out2, int ol,
+           uint8_t *in_gold, uint8_t *in1, uint8_t *in2, int il,
+           const char *string)
+{
+    uint64_t a, b;
+
+    // printf("%s:\n", string);
+    for (int i = 0; i < il; i++)
+    {
+        a = rand();
+        b = rand();
+        a = (a << 32) | rand();
+        b = (b << 32) | rand();
+        in1[i] = a;
+        in2[i] = b;
+        in_gold[i] = a;
+    }
+
+    long_long start, end;
+    start = PAPI_get_real_cyc();
+    for (int j = 0; j < TESTS; j++)
+    {
+        funcx2(out1, out2, ol, in1, in2, il);
+    }
+    end = PAPI_get_real_cyc();
+    // printf("NEON: %lld/operation\n", (end - start)/TESTS);
+    printf("NEON: %f cyc/operation\n", ((double)(end - start)) / TESTS);
+
+    start = PAPI_get_real_cyc();
+    for (int j = 0; j < TESTS; j++)
+    {
+        func(out_gold, ol, in_gold, il);
+    }
+    end = PAPI_get_real_cyc();
+    // printf("FIPS: %lld/operation\n", (end - start)/TESTS);
+    printf("FIPS: %f cyc/operation\n", ((double)(end - start)) / TESTS);
+
+    if (compare(out_gold, out1, out2, ol))
+        return 1;
+
+    return 0;
+}
+
+int bench_shake128()
+{
     uint8_t out1[OUTLENGTH], out2[OUTLENGTH];
     uint8_t in1[INLENGTH], in2[INLENGTH];
     uint8_t out_gold[OUTLENGTH], in_gold[INLENGTH];
 
-    uint64_t bla, blo;
+    int ret;
+    void (*func)()   = &shake128, 
+         (*funcx2)() = &shake128x2;
 
-    for (int i = 0; i < INLENGTH; i++)
-    {
-        bla = rand();
-        blo = rand();
-        in1[i] = bla;
-        in2[i] = blo;
-        in_gold[i] = bla;
-        // in_gold[i] = blo;
-    }
-    long_long start, end;
-
-#if BRUTE
     for (int ol = 1; ol < OUTLENGTH; ol++)
     {
         for (int il = 1; il < INLENGTH; il++)
         {
-#else
-    int ol = OUTLENGTH;
-    int il = INLENGTH;
-#endif 
-            start = PAPI_get_real_cyc();
-            for (int j = 0; j < TESTS; j++)
+            ret = bench(func, funcx2, out_gold, out1, out2, ol, in_gold, in1, in2, il, "BENCHMARK SHAKE128");
+            printf("[%d, %d]\n", ol, il);
+            if (ret)
             {
-                shake128x2(out1, out2, ol, in1, in2, il);
+                printf("%d %d: ERROR\n", ol, il);
+                return 1;
             }
-            end = PAPI_get_real_cyc();
-            // printf("NEON: %lld/operation\n", (end - start)/TESTS);
-            printf("NEON: %f cyc/operation\n", ((double) (end - start) )/TESTS);
-
-            start = PAPI_get_real_cyc();
-            for (int j = 0; j < TESTS; j++)
-            {
-                shake128(out_gold, ol, in_gold, il);
-            }
-            end = PAPI_get_real_cyc();
-            // printf("FIPS: %lld/operation\n", (end - start)/TESTS);
-            printf("FIPS: %f cyc/operation\n", ((double) (end - start) )/TESTS);
-
-            int check = 0;
-            uint8_t m, n;
-
-            // for (int i = 0; i < OUTLENGTH - 8; i += 8)
-            for (int i = 0; i < ol - 8; i += 8)
-            {
-                for (int j = i; j < i + 8; j++)
-                {
-                    m = out_gold[j];
-                    n = out1[j];
-                    if (out_gold[j] != out1[j])
-                    {
-                        printf("![%2d] %2x != %2x  ?? %2x\n", j, m, n, out2[j]);
-                        check++;
-                    }
-                    else
-                    {
-                        // printf("+[%2d] %2x == %2x ?? %2x\n", j, m, n, out2[j]);
-                    }
-                }
-
-                if (check)
-                {
-                    printf("%d %d ERROR! %d\n", ol, il, i);
-                    // printf("ERROR! %d\n", i);
-                    return 1;
-                }
-            }
-#if BRUTE
         }
     }
-#endif 
-
-    printf("CORRECT!\n");
+    printf("ALL CORRECT!\n");
+    printf("============\n");
 
     return 0;
+}
+
+int bench_shake256()
+{
+    uint8_t out1[OUTLENGTH], out2[OUTLENGTH];
+    uint8_t in1[INLENGTH], in2[INLENGTH];
+    uint8_t out_gold[OUTLENGTH], in_gold[INLENGTH];
+
+    int ret;
+    void (*func)()   = &shake256, 
+        (*funcx2)() = &shake256x2;
+
+    for (int ol = 1; ol < OUTLENGTH; ol++)
+    {
+        for (int il = 1; il < INLENGTH; il++)
+        {
+            ret = bench(func, funcx2, out_gold, out1, out2, ol, in_gold, in1, in2, il, "BENCHMARK SHAKE256");
+            printf("[%d, %d]\n", ol, il);
+            if (ret)
+            {
+                printf("%d %d: ERROR\n", ol, il);
+                return 1;
+            }
+        }
+    }
+    printf("ALL CORRECT!\n");
+    printf("============\n");
+
+    return 0;
+}
+
+int main()
+{
+    srand(time(0));
+
+    int ret = 0;
+    printf("BENCHMARK SHAKE128:\n");
+    ret |= bench_shake128();
+    printf("BENCHMARK SHAKE256:\n");
+    ret |= bench_shake256();
+
+    return ret;
 }
